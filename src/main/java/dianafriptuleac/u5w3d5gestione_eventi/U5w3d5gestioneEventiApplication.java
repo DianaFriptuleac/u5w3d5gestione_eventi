2c1ee60894b23971af6a5efe4c71989011f26dfc
Diana <dianadorojuc3@gmail.com>
2024-11-08 10:11:42 +0100

    Initial commit

---
+++ src/main/java/dianafriptuleac/u5w3d5gestione_eventi/U5w3d5gestioneEventiApplication.java
@@ -0,0 +1,13 @@
+package dianafriptuleac.u5w3d5gestione_eventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w3d5gestioneEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w3d5gestioneEventiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dianafriptuleac/u5w3d5gestione_eventi/U5w3d5gestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package dianafriptuleac.u5w3d5gestione_eventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w3d5gestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
